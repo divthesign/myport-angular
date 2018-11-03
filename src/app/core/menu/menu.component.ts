3e125c42d38917338878ae4c2d50352d0cd16f6c
daniel <verdudaniel7@gmail.com>
2018-11-03 21:14:54 +0100

    problema de compilacion arreglado y *ngIf

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'div-menu',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+    
   }
+
 
 }
